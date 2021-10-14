d920c49dfb0954615b10b82382a6f51afe8dc252
kribow21 <kristininnotech@gmail.com>
2021-10-14 17:16:28 -0600

    completed tweet patch with proper validation

--- tweeter/tweets.py
+++ tweeter/tweets.py
@@ -139,3 +139,73 @@ def tweets():
             print('connection closed')
         else:
             print('the connection never opened, nothing to close')
+    if request.method == "PATCH":
+        data = request.json
+        tweet_token = data.get("loginToken")
+        tweet_id = data.get("tweetId")
+        tweet_content = data.get("content")
+        patch_fail = {
+            "message" : "something went wrong with editing your tweet"
+        }
+        if_empty = {
+            "message" : "Enter in required data"
+        }
+        if (tweet_id == ''):
+                    return Response(json.dumps(if_empty, default=str),
+                                mimetype='application/json',
+                                status=409)
+        if (tweet_content == ''):
+                    return Response(json.dumps(if_empty, default=str),
+                                mimetype='application/json',
+                                status=409)
+        if (len(tweet_token) != 32):
+            return Response(json.dumps(patch_fail, default=str),
+                                mimetype='application/json',
+                                status=409)
+    try:
+        conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute("SELECT user_id FROM user_session WHERE login_token=?",[tweet_token,])
+        session_userId = cursor.fetchone()
+        cursor.execute("SELECT user_id FROM tweet WHERE id=?",[tweet_id,])
+        tweet_userId = cursor.fetchone()
+        if (session_userId == tweet_userId):
+            cursor.execute("UPDATE tweet SET content=? WHERE id=?",[tweet_content,tweet_id])
+            conn.commit()
+            cursor.execute("SELECT id, content FROM tweet WHERE id=?",[tweet_id,])
+            edited_tweet = cursor.fetchone()
+            response = {
+                "tweetId" : edited_tweet[0],
+                "content" : edited_tweet[1]
+            }
+            return Response(json.dumps(response, default=str),
+                                        mimetype='application/json',
+                                        status=200)
+        else:
+            return Response(json.dumps(patch_fail, default=str),
+                                mimetype='application/json',
+                                status=409)
+    except mariadb.DataError: 
+        print('Something went wrong with your data')
+    except mariadb.OperationalError:
+        print('Something wrong with the connection')
+    except mariadb.ProgrammingError:
+        print('Your query was wrong')
+    except mariadb.IntegrityError:
+        print('Your query would have broken the database and we stopped it')
+    except mariadb.InterfaceError:
+        print('Something wrong with database interface')
+    except:
+        print('Something went wrong')
+    finally:
+        if(cursor != None):
+            cursor.close()
+            print('cursor closed')
+        else:
+            print('no cursor to begin with')
+        if(conn != None):   
+            conn.rollback()
+            conn.close()
+            print('connection closed')
+        else:
+            print('the connection never opened, nothing to close')
