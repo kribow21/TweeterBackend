9673346d8f30f30a58e5d3f62142063ea4892eed
kribow21 <kristininnotech@gmail.com>
2021-10-18 17:15:48 -0600

    complete comment like get request

--- tweeter/commentlikes.py
+++ tweeter/commentlikes.py
@@ -70,3 +70,68 @@ def commentlikes():
                 print('connection closed')
             else:
                 print('the connection never opened, nothing to close')
+    
+    if request.method == "GET":
+        params = request.args
+        comment_ID = params.get("commentId")
+    try:
+        if (comment_ID == None):
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT comment_like.comment_id , comment_like.user_id,user.username FROM comment_like INNER JOIN user ON comment_like.user_id=user.id")
+            everyones_coms = cursor.fetchall()
+            like_coll = []
+            for like in everyones_coms:
+                collection = {
+                        "commentId" : like[0],
+                        "userId" : like[1],
+                        "username" : like[2]
+                    }
+                like_coll.append(collection)
+            return Response(json.dumps(like_coll, default=str),
+                                    mimetype='application/json',
+                                    status=200)
+        elif (len(params) == 1):
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT comment_like.comment_id , comment_like.user_id,user.username FROM comment_like INNER JOIN user ON comment_like.user_id=user.id WHERE comment_id=?",[comment_ID,])
+            all_likes = cursor.fetchall()
+            com_likes = []
+            for likes in all_likes:
+                resp = {
+                    "tweetId" : likes[0],
+                    "userId" : likes[1],
+                    "username" : likes[2]
+                }
+                com_likes.append(resp)
+            return Response(json.dumps(com_likes, default=str),
+                                mimetype='application/json',
+                                status=200)
+        else:
+            return Response(json.dumps(data_error, default=str),
+                                mimetype='application/json',
+                                status=409)
+    except mariadb.DataError: 
+        print('Something went wrong with your data')
+    except mariadb.OperationalError:
+        print('Something wrong with the connection')
+    except mariadb.ProgrammingError:
+        print('Your query was wrong')
+    except mariadb.IntegrityError:
+        print('Your query would have broken the database and we stopped it')
+    except mariadb.InterfaceError:
+        print('Something wrong with database interface')
+    except:
+        print('Something went wrong')
+    finally:
+        if(cursor != None):
+            cursor.close()
+            print('cursor closed')
+        else:
+            print('no cursor to begin with')
+        if(conn != None):   
+            conn.rollback()
+            conn.close()
+            print('connection closed')
+        else:
+            print('the connection never opened, nothing to close')
