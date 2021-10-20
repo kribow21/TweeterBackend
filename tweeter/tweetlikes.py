d6033f8ed0fea62a4af9661880a07a4c0c1eb239
kribow21 <kristininnotech@gmail.com>
2021-10-19 19:58:22 -0600

    fixed format

--- tweeter/tweetlikes.py
+++ tweeter/tweetlikes.py
@@ -13,66 +13,66 @@ def tweetlikes():
             "message" : "invalid data sent"
         }
     if request.method == "POST":
-        try:
-            data = request.json
-            tweetID = data.get("tweetId")
-            repeat = {
-                "message" : "conflict, user already liked"
-            }
-            resp = {
-                "message" : "like OK"
-            }
-            token = data.get("loginToken")
-            if (len(token) == 32 and isinstance(tweetID, int) == True):
-                conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
-                cursor = conn.cursor()
-                cursor.execute("SELECT user_id FROM user_session WHERE login_token=?",[token,])
-                user_id = cursor.fetchone()
-                cursor.execute("SELECT * FROM tweet_like WHERE tweet_id=? AND user_id=?",[tweetID, user_id[0]])
-                already_liked = cursor.fetchone()
-            # checking if those two inputs already exsist in the db , if not it returns none if they do exsist the variable will have a length of 2
-                if(already_liked == None):
-                    cursor.execute("INSERT INTO tweet_like(tweet_id, user_id) VALUES (?,?)",[tweetID, user_id[0]])
-                    conn.commit()
-                    return Response(json.dumps(resp, default=str),
-                                        mimetype='application/json',
-                                        status=200)
-                #if it hits this the like already exsists
-                elif(len(already_liked) == 2):
-                    return Response(json.dumps(repeat, default=str),
+        data = request.json
+        tweetID = data.get("tweetId")
+        repeat = {
+            "message" : "conflict, user already liked"
+        }
+        resp = {
+            "message" : "like OK"
+        }
+    try:
+        token = data.get("loginToken")
+        if (len(token) == 32 and isinstance(tweetID, int) == True):
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT user_id FROM user_session WHERE login_token=?",[token,])
+            user_id = cursor.fetchone()
+            cursor.execute("SELECT * FROM tweet_like WHERE tweet_id=? AND user_id=?",[tweetID, user_id[0]])
+            already_liked = cursor.fetchone()
+        # checking if those two inputs already exsist in the db , if not it returns none if they do exsist the variable will have a length of 2
+            if(already_liked == None):
+                cursor.execute("INSERT INTO tweet_like(tweet_id, user_id) VALUES (?,?)",[tweetID, user_id[0]])
+                conn.commit()
+                return Response(json.dumps(resp, default=str),
                                     mimetype='application/json',
-                                    status=409)
-                # returns if the user sent in data that does not match the if statement about the token being 32 and the tweetid being a int
-            else:
-                return Response(json.dumps(data_error, default=str),
-                                    mimetype='application/json',
-                                    status=409)
-        except mariadb.DatabaseError:
-            print('Something went wrong with connecting to database')
-        except mariadb.DataError: 
-            print('Something went wrong with your data')
-        except mariadb.OperationalError:
-            print('Something wrong with the connection')
-        except mariadb.ProgrammingError:
-            print('Your query was wrong')
-        except mariadb.IntegrityError:
-            print('Your query would have broken the database and we stopped it')
-        except mariadb.InterfaceError:
-            print('Something wrong with database interface')
-        except:
-            print('Something went wrong')
-        finally:
-            if(cursor != None):
-                cursor.close()
-                print('cursor closed')
-            else:
-                print('no cursor to begin with')
-            if(conn != None):   
-                conn.rollback()
-                conn.close()
-                print('connection closed')
-            else:
-                print('the connection never opened, nothing to close')
+                                    status=200)
+            #if it hits this the like already exsists
+            elif(len(already_liked) == 2):
+                return Response(json.dumps(repeat, default=str),
+                                mimetype='application/json',
+                                status=409)
+            # returns if the user sent in data that does not match the if statement about the token being 32 and the tweetid being a int
+        else:
+            return Response(json.dumps(data_error, default=str),
+                                mimetype='application/json',
+                                status=409)
+    except mariadb.DatabaseError:
+        print('Something went wrong with connecting to database')
+    except mariadb.DataError: 
+        print('Something went wrong with your data')
+    except mariadb.OperationalError:
+        print('Something wrong with the connection')
+    except mariadb.ProgrammingError:
+        print('Your query was wrong')
+    except mariadb.IntegrityError:
+        print('Your query would have broken the database and we stopped it')
+    except mariadb.InterfaceError:
+        print('Something wrong with database interface')
+    except:
+        print('Something went wrong')
+    finally:
+        if(cursor != None):
+            cursor.close()
+            print('cursor closed')
+        else:
+            print('no cursor to begin with')
+        if(conn != None):   
+            conn.rollback()
+            conn.close()
+            print('connection closed')
+        else:
+            print('the connection never opened, nothing to close')
 
     if request.method == "GET":
         params = request.args
